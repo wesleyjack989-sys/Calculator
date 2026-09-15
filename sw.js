08d7f56af5a59d92487e967659b058840044bace
wesleyjack989-sys <wesleyjack989@gmail.com>
2026-09-15 14:49:19 +0200

    Refactor service worker install event listener

--- sw.js
+++ sw.js
@@ -1,6 +1,5 @@
 self.addEventListener("install", e => {
-    console.log("Installing sw");
-	console.log("Caching resources..");
+    console.log("Caching resources..");
     e.waitUntil(
         caches.open("static").then(cache => {
             return cache.addAll([
@@ -12,3 +11,12 @@ self.addEventListener("install", e => {
     );
     console.log("Resources in cache. Done");
 })
+
+self.addEventListener("fetch", e => {
+    console.log('intercepting fecth request for:', e.request.url);
+    e.respondWith(
+        caches.match(e.request).then(response => {
+            return response || fetch(e.request);
+        })
+    )
+});
